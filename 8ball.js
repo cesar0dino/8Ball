e90e98fa5dba6ea41916bda1e8648199a2122ba1
de6y <de6y@fresnou.org>
2023-04-21 09:48:43 -0700

    8ball

--- 8ball.js
+++ 8ball.js
@@ -1,5 +1,17 @@
-let answerList = ['yes', 'no', 'maybe'];
+const readLine = require(`readline-sync`);
 
-let randomNum = Math.floor( Math.random()*answerList.length);
+let answer = readLine.question("Welcome do you want to know your fortune? y or n: ");
+while (answer != "y" && answer != "n"){
+    console.log("invalid choice");
+    answer = readLine.question("Enter y or n: ");
+}
+let answer2 = readLine.question("Ask your question: ");
+
+
+
+let answerList = ['Yes.', 'Definitely.', 'No.', 'My sources say no.', 'Ask again later.', 'Try again'];
+
+let randomNum = Math.floor(Math.random()*answerList.length);
+
 let randomAnswer = answerList[randomNum];
-console.log(randomAnswer);
+console.log(randomAnswer)
